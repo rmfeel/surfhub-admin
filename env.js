5a4b7c646a99127150ac63a723f5bcc9fb68f1ba
Kwan <rmemotion@gmail.com>
2020-11-16 01:13:51 +0800

    first commit

---
+++ env.js
@@ -0,0 +1,16 @@
+window.settings = {
+  // 站点标题
+  title: 'Surfhub',
+  // 站点描述
+  description: '助你快速连接全球联网',
+  // API
+  host: 'https://api.surfhub.net',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'light',
+    color: 'darkblue'
+  },
+  // 背景
+  background_url: '//cdn.jsdelivr.net/gh/rmfeel/cdn@main/img/ruben.jpg'
+}
